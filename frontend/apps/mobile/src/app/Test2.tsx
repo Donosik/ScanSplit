6d49d6e4cd0f30b4c9cf29bd56b934c374b6f9fa
Damian <damian1003@onet.pl>
2024-11-09 23:47:54 +0100

    added react-router-native for mobile app

--- frontend/apps/mobile/src/app/Test2.tsx
+++ frontend/apps/mobile/src/app/Test2.tsx
@@ -1,12 +1,15 @@
 import { AuthContextProps, useAuth } from '@frontend/providers';
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'react-router-native';
 
-export default function Test2()
-{
+export default function Test2() {
   const auth: AuthContextProps = useAuth();
 
   return (
+    <View>
+      <Link to={'/login'} ><Text>zaloguj</Text></Link>
       <Text>{auth.isAuthenticated ? 'Zalogowany' : 'Niezalogowany'}</Text>
-  )
+    </View>
+  );
 }
