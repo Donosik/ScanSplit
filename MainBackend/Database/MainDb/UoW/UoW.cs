36548cbeddb67fa6937075d28305cafa898931a6
Damian <damian1003@onet.pl>
2024-11-18 21:52:09 +0100

    changed constructor of Uow

--- MainBackend/Database/MainDb/UoW/UoW.cs
+++ MainBackend/Database/MainDb/UoW/UoW.cs
@@ -7,29 +7,6 @@ public class UoW : IUoW
 {
     private readonly DB.Context.MainDb context;
 
-    public UoW(DB.Context.MainDb context, IUserRepository userRepository, ITransferRepository transferRepository,
-        IBillRepository billRepository, IGroupRepository groupRepository, ILocationRepository locationRepository,
-        IMenuItemRepository menuItemRepository, IPaymentRepository paymentRepository)
-    {
-        //this.context = context;
-        //this.userRepository = userRepository;
-        //this.transferRepository = transferRepository;
-        //this.billRepository = billRepository;
-        //this.groupRepository = groupRepository;
-        //this.locationRepository = locationRepository;
-        //this.menuItemRepository = menuItemRepository;
-        //this.paymentRepository = paymentRepository;
-
-        this.context = context ?? throw new ArgumentNullException(nameof(context));
-        UserRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
-        TransferRepository = transferRepository ?? throw new ArgumentNullException(nameof(transferRepository));
-        BillRepository = billRepository ?? throw new ArgumentNullException(nameof(billRepository));
-        GroupRepository = groupRepository ?? throw new ArgumentNullException(nameof(groupRepository));
-        LocationRepository = locationRepository ?? throw new ArgumentNullException(nameof(locationRepository));
-        MenuItemRepository = menuItemRepository ?? throw new ArgumentNullException(nameof(menuItemRepository));
-        PaymentRepository = paymentRepository ?? throw new ArgumentNullException(nameof(paymentRepository));
-    }
-
     public IUserRepository UserRepository { get; }
     public ITransferRepository TransferRepository { get; }
     public IBillRepository BillRepository { get; }
@@ -37,6 +14,21 @@ public class UoW : IUoW
     public ILocationRepository LocationRepository { get; }
     public IMenuItemRepository MenuItemRepository { get; }
     public IPaymentRepository PaymentRepository { get; }
+    
+    public UoW(DB.Context.MainDb context, IUserRepository userRepository, ITransferRepository transferRepository,
+        IBillRepository billRepository, IGroupRepository groupRepository, ILocationRepository locationRepository,
+        IMenuItemRepository menuItemRepository, IPaymentRepository paymentRepository)
+    {
+        this.context = context;
+        this.UserRepository = userRepository;
+        this.TransferRepository = transferRepository;
+        this.BillRepository = billRepository;
+        this.GroupRepository = groupRepository;
+        this.LocationRepository = locationRepository;
+        this.MenuItemRepository = menuItemRepository;
+        this.PaymentRepository = paymentRepository;
+        
+    }
 
     public async Task Save(int entities = 1)
     {
