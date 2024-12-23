64326a483f5835b6992ccc3e755dfdb78be7f403
Damian <damian1003@onet.pl>
2024-12-23 01:13:23 +0100

    finished look of register page

--- frontendweb/src/pages/register/Register.tsx
+++ frontendweb/src/pages/register/Register.tsx
@@ -1,11 +1,17 @@
 import RegisterForm from "./registerForm/RegisterForm.tsx";
 import style from "./Register.module.css"
+import {registerDTO} from "../../utils/services/authService.ts";
 
 export default function Register()
 {
+    function onSubmit(data:registerDTO)
+    {
+        console.log(data);
+    }
+
     return (
         <div className={style.container}>
-            <RegisterForm/>
+            <RegisterForm onSubmit={onSubmit}/>
         </div>
     )
 }
