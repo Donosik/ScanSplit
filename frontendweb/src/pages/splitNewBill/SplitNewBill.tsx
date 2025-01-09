d95603e37fd9427fd4594af115e036cd309325c2
Damian <damian1003@onet.pl>
2025-01-09 22:31:21 +0100

    added split new bill with route and component and module style file for Magda to work on

---
+++ frontendweb/src/pages/splitNewBill/SplitNewBill.tsx
@@ -0,0 +1,10 @@
+import style from "./SplitNewBill.module.css"
+
+export default function SplitNewBill()
+{
+    return(
+        <div className={style.container}>
+            SplitNewBill
+        </div>
+    )
+}
